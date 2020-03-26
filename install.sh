e6e6c19af84a4bca6815487d93e2066c45961ad5
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-26 19:00:45 -0400

    Update and rename installGoodCommands to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
-cp Deleteinteractive /usr/bin/delete
+cp Deleteinteractive /usr/bin/delete #copy file Deleteinteractive to folder /usr/bin and rename it to delete
 cp pokeydunnet /usr/bin/dunnet
 cp Rsync /usr/bin/sync
 cp goodiecommand /usr/bin/goodiecommand
 cp InstallInteractive /usr/bin/install
 
-chmod +x /usr/bin/delete
+chmod +x /usr/bin/delete #make /usr/bin/delete executable (so that you can run it)
 chmod +x /usr/bin/dunnet
 chmod +x /usr/bin/sync
 chmod +x /usr/bin/goodiecommand
 chmod +x /usr/bin/install
-
-#Commented ones may not be used
